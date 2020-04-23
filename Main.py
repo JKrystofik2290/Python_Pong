ea3a3d17665065078e83c212f8f0edc673f1b269
Krystofik <joe.krystofik@crown.com>
2020-04-23 08:01:54 -0400

    added Main.py

---
+++ Main.py
@@ -0,0 +1,35 @@
+import pygame, sys
+
+# init
+pygame.init()
+clock = pygame.time.Clock()
+screen_x = 1280
+screen_y = 960
+screen = pygame.display.set_mode((screen_x,screen_y))
+pygame.display.set_caption("Python Pong")
+running = True
+# pygame.Rect(left, top, x, y)
+ball = pygame.Rect(screen_x/2 - 15, screen_y/2 - 15, 30, 30)
+player = pygame.Rect(screen_x - 20, screen_y/2 - 70, 10, 140)
+opponent = pygame.Rect(20, screen_y/2 - 70, 10, 140)
+
+# pygame.draw(surface, color, rect)
+pygame.draw(screen, (0,0,0), ball)
+
+# Main Loop
+while running:
+
+    # event handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+
+
+
+
+    # screen update
+    pygame.display.flip()
+    clock.tick(60)
